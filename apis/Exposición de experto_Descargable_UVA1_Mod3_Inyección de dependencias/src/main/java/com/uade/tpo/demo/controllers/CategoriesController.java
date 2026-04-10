31bb2a75d9153c0eb7e261a12302cb30f7adef30
martinschauvinhold <mschauvinhold@uade.edu.ar>
2026-04-10 09:22:24 -0300

    Add files via upload

---
+++ apis/Exposición de experto_Descargable_UVA1_Mod3_Inyección de dependencias/src/main/java/com/uade/tpo/demo/controllers/CategoriesController.java
@@ -0,0 +1,70 @@
+package com.uade.tpo.demo.controllers;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uade.tpo.demo.entity.Category;
+import com.uade.tpo.demo.entity.dto.CategoryRequest;
+import com.uade.tpo.demo.exceptions.CategoryDuplicateException;
+import com.uade.tpo.demo.service.CategoryService;
+
+@RestController
+@RequestMapping("categories")
+public class CategoriesController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<ArrayList<Category>> getCategories() {
+        return ResponseEntity.ok(categoryService.getCategories());
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable int categoryId) {
+        Optional<Category> result = categoryService.getCategoryById(categoryId);
+        if (result.isPresent())
+            return ResponseEntity.ok(result.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createCategory(@RequestBody CategoryRequest categoryRequest)
+            throws CategoryDuplicateException {
+        Category result = categoryService.createCategory(categoryRequest.getDescription());
+        return ResponseEntity.created(URI.create("/categories/" + result.getId())).body(result);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<Object> updateCategory(@PathVariable int categoryId,
+            @RequestBody CategoryRequest categoryRequest) {
+        Optional<Category> result = categoryService.getCategoryById(categoryId);
+        if (result.isPresent()) {
+            Category updated = categoryService.updateCategory(categoryId, categoryRequest.getDescription());
+            return ResponseEntity.ok(updated);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Object> deleteCategory(@PathVariable int categoryId) {
+        Optional<Category> result = categoryService.getCategoryById(categoryId);
+        if (result.isPresent()) {
+            categoryService.deleteCategory(categoryId);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ apis/Exposición de experto_Descargable_UVA1_Mod3_Inyección de dependencias/src/main/java/com/uade/tpo/demo/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uade.tpo.demo.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByDescription(String description);
+}
